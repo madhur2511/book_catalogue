251063671b8f9605973e481da02551ff11976d45
Madhur Kapoor <madhur2511@gmail.com>
2013-06-10 23:48:02 +0530

    v2.1

--- src/scripts/routers/router.js
+++ src/scripts/routers/router.js
@@ -11,40 +11,65 @@ define(['views/AppView','views/CatalogView','views/ThumbView','views/BookView','
 		initialize:function(stuff){
 			console.log('Router Initialized and Running');
 			window.App = {};										//creating namespace for the App.
-
-			App.AppView = new AppView();
-
-			App.catalog = new Catalog([
-		      new Book({name : "A",author : "A Bc",description : "abc",category : "abc" ,comments:[],url:"/home/madhur/Desktop/Artoo/Book Catalog/assets/A.png"}),
-		      new Book({name : "B",author : "D Ef",description : "def",category : "def" ,comments:[],url:"/home/madhur/Desktop/Artoo/Book Catalog/assets/B.jpg"}),
-		      new Book({name : "C",author : "G Hi",description : "ghi",category : "ghi" ,comments:[],url:"/home/madhur/Desktop/Artoo/Book Catalog/assets/C.png"}),
-		      new Book({name : "D",author : "J Kl",description : "jkl",category : "jkl" ,comments:[],url:"/home/madhur/Desktop/Artoo/Book Catalog/assets/D.jpg"}),
-		      new Book({name : "E",author : "M No",description : "mno",category : "mno" ,comments:[],url:"/home/madhur/Desktop/Artoo/Book Catalog/assets/E.png"}),
-		      new Book({name : "F",author : "P Qr",description : "pqr",category : "pqr" ,comments:[],url:"/home/madhur/Desktop/Artoo/Book Catalog/assets/F.jpg"})
-		      ]);
+			App.DATABASE = "http:localhost:5984/books/";
+			App.AppView = new AppView();			
 		},
 
 		index:function(){
-			App.AppView.clearContainer();
-			console.log("Showing Catalog View");
-			App.AppView.appendChild(new CatalogView({collection:App.catalog}));
+			$.ajax({
+				url : App.DATABASE+"_design/all/_view/all",
+				async:true,
 
+				success:function(JSONresult){
+					books=[];
+					obj=JSON.parse(JSONresult);
+					if(obj['rows']) 
+						for(i=0;i<obj['rows'].length;++i)
+							books.push(obj['rows'][i]['value']);
+					
+					App.catalog = new Catalog([
+						new Book(books[0]),
+						new Book(books[1]),
+						new Book(books[2]),
+						new Book(books[3]),
+						new Book(books[4]),
+						new Book(books[5])
+					]);
+				},
+
+				complete:function(){
+					console.log("Showing Catalog View");
+					App.AppView.clearContainer();
+					App.AppView.appendChild(new CatalogView({collection:App.catalog}));		
+				}
+
+			});
 		},
 
 		showonly:function(id){
-			App.AppView.clearContainer();
-			console.log("Showing Book View");
+			$.ajax({
+				url : App.DATABASE + id,
+				async:true,
 
-			for(i = 0 ; i < App.catalog.length ; ++i){
-	        	if(App.catalog.at(i).get('name') === id) {
-	        		model = App.catalog.at(i);
-	        		break; 
-	        	}
-	      	}
+				success:function(JSONresult){
+					books=[];
+					obj=JSON.parse(JSONresult);
+					book=[];
+					for(i in obj){
+						if(i != '_id' || i != '_rev'){
+							book[i]=obj[i];
+						}
+					}
+					App.model = new Book(book);
+				},
 
-			App.AppView.appendChild(new BookView({model:model}));
+				complete:function(){
+					console.log("Showing Book View");
+					App.AppView.clearContainer();
+					App.AppView.appendChild(new BookView({model:App.model}));
+				}
+			});	
 		}
-
 	});
 
 	return Router;
